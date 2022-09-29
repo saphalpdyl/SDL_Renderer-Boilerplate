88fc5fc63313ee265cd7204068ef93eac3c3896f
saphalpdyl <saphalpdyl@gmail.com>
2022-09-29 10:40:01 +0545

    ADDED : A simple game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
@@ -16,12 +17,24 @@ int main()
     // Creating and initializing required components
     SDL_Func::initializeEverything( "Texture & Keyboard Event" , gWindow , gRenderer , IMG_INIT_FLAGS );
 
-	SDL_SetRenderDrawColor( gRenderer , 0xFF , 0 , 0 , 0xFF );
-	SDL_Rect fillRect = { SCREEN_WIDTH / 4 , SCREEN_HEIGHT / 4 , SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 };
-	SDL_RenderFillRect( gRenderer , &fillRect );
-	SDL_RenderPresent( gRenderer );
+	bool gameLoopRunning = true;
+	SDL_Event event;
 
-	SDL_Delay( 3000 );
+	// Simple Game Loop
+	while ( gameLoopRunning )
+	{
+		while ( SDL_PollEvent( &event ) )
+		{
+			if ( event.type == SDL_QUIT || event.type == SDL_KEYDOWN ) // Quit on any button pressed or clicked exit
+				gameLoopRunning = false;
+		}
+
+		SDL_SetRenderDrawColor( gRenderer , 0xFF , 0 , 0 , 0xFF );
+		SDL_Rect fillRect = { SCREEN_WIDTH / 4 , SCREEN_HEIGHT / 4 , SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 };
+		SDL_RenderFillRect( gRenderer , &fillRect );
+		SDL_RenderPresent( gRenderer );
+	}
+
 
     SDL_Func::close(gWindow , gRenderer , nullptr , 0);
     return RETURN_SUCCESS;
